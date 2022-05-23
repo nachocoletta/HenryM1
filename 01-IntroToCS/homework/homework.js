8c78b368f6be9c34e5055d3363d9dec86ff1d719
Nacho <ignacio_coletta@hotmail.com>
2022-05-23 18:21:19 -0300

    intro to cs homework done

---
+++ 01-IntroToCS/homework/homework.js
@@ -0,0 +1,40 @@
+'use strict'
+
+function BinarioADecimal(num) {
+  // tu codigo aca
+  let acumulado = 0;
+	var numeroEnBinarioString = String(num);
+  let j = 0;
+  
+  for(let i = numeroEnBinarioString.length; i > 0; i--){
+    acumulado = acumulado + (parseInt(numeroEnBinarioString[j]) * Math.pow(2,i-1));
+    j++;
+  }
+  
+	return acumulado;
+}
+
+function DecimalABinario(num) {
+  // tu codigo aca
+  let numeroBinario = "";
+  let resto;
+  
+   while(num / 2 !== 0) {
+      resto = num % 2;
+      console.log(resto);
+      num = Math.floor(num / 2);
+      numeroBinario += resto;
+   } 
+   
+  let arreglo = [];
+  arreglo =  numeroBinario.split('').reverse().join('');
+  
+  
+  return arreglo;
+}
+
+
+module.exports = {
+  BinarioADecimal,
+  DecimalABinario,
+}
