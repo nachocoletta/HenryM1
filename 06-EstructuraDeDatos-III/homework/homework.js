fb360b40f58269d7069b222a35af99a7a1aedd97
Nacho <ignacio_coletta@hotmail.com>
2022-05-31 23:08:01 -0300

    homeworks

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -5,13 +5,133 @@
   - size: retorna la cantidad total de nodos del árbol
   - insert: agrega un nodo en el lugar correspondiente
   - contains: retorna true o false luego de evaluar si cierto valor existe dentro del árbol
-  - depthFirstForEach: recorre el árbol siguiendo el orden depth first (DFS) en cualquiera de sus variantes, según se indique por parámetro ("post-order", "pre-order", o "in-order"). Nota: si no se provee ningún parámetro, hará el recorrido "in-order" por defecto.
+  - depthFirstForEach: recorre el árbol siguiendo el orden depth first (DFS) en cualquiera de sus variantes,
+   según se indique por parámetro ("post-order", "pre-order", o "in-order"). 
+   Nota: si no se provee ningún parámetro, hará el recorrido "in-order" por defecto.
   - breadthFirstForEach: recorre el árbol siguiendo el orden breadth first (BFS)
 
-  El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
+  El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro 
+  del directorio homework.
 */
 
-function BinarySearchTree() {}
+function BinarySearchTree(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+
+BinarySearchTree.prototype.size = function () {
+  if(this.right === null && this.left === null){
+    return 1;
+  }
+  if(this.right === null && this.left !== null){
+    return 1 + this.left.size();
+  }
+  if(this.right !== null && this.left === null){
+    return 1 + this.right.size();
+  }
+  if(this.right !== null && this.left !== null){
+    return 1 + this.right.size () + this.left.size();
+  }
+}
+
+BinarySearchTree.prototype.insert = function (value){
+  if(this.value > value){
+    if(this.left !== null){
+      this.left.insert(value);
+    }
+    else{
+      this.left = new BinarySearchTree(value);
+    }
+  }
+
+  if(this.value < value){
+    if(this.right !== null){
+      this.right.insert(value);
+    }
+    else {
+      this.right = new BinarySearchTree(value);
+    }
+  }
+}
+
+
+BinarySearchTree.prototype.contains = function (value){
+  if(this.value === value){ // si esta en el root retorno true, es el primer elemento del tree
+    return true;
+  }
+  if(this.value > value){
+   if(this.left !== null){
+     return this.left.contains(value);
+   }
+   else {
+     return false;
+   }
+  }
+
+  if(this.value < value){
+    if(this.right !== null){
+      return this.right.contains(value);
+    }
+    else {
+      return false;
+    }
+  }
+}
+
+BinarySearchTree.prototype.depthFirstForEach = function (cb, order){
+ // "post-order", "pre-order", o "in-order"
+  switch(order){
+    case "post-order": // izq - dere - root
+      if(this.left !== null){
+        this.left.depthFirstForEach(cb, order);
+      }
+      if(this.right !== null){
+        this.right.depthFirstForEach(cb, order);
+      }
+      cb(this.value);
+      break;
+
+    case "pre-order":
+      cb(this.value);
+      if(this.left !== null){
+        this.left.depthFirstForEach(cb, order);
+      }
+      if(this.right !== null){
+        this.right.depthFirstForEach(cb, order);
+      }
+      break;
+
+    default:  // "in-order"
+      if(this.left !== null){
+        this.left.depthFirstForEach(cb, order);
+      }
+      cb(this.value);
+      if(this.right !== null){
+        this.right.depthFirstForEach(cb, order);
+      }
+      break;
+  }
+
+}
+
+BinarySearchTree.prototype.breadthFirstForEach = function (cb, array = []){ // por niveles
+  
+  cb(this.value);
+
+  if(this.left !== null){
+    array.push(this.left);
+  }
+
+  if(this.right !== null){
+    array.push(this.right);
+  }
+
+  if(array.length > 0){
+    array.shift().breadthFirstForEach(cb, array);
+  }
+}
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -7,6 +7,20 @@ function factorear(num) {
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
 
+  let array = [1];
+
+  let aux = 2;
+
+  while(num > 1){
+    if(num % aux === 0){
+      array.push(aux);
+      num = num / aux;
+    }else{
+      aux++;
+    }
+  }
+  return array;
+
 }
 
 function bubbleSort(array) {
@@ -15,6 +29,18 @@ function bubbleSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+    let aux;
+
+    for(let i = 0; i < array.length; i++){
+      for(let j = i+1; j < array.length; j++){
+        if(array[i] > array[j]){
+          aux = array[i];
+          array[i] = array[j];
+          array[j] = aux; 
+        }
+      }
+    }
+    return array;
 }
 
 
@@ -22,8 +48,19 @@ function insertionSort(array) {
   // Implementar el método conocido como insertionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
-  // Tu código:
+  // Tu código: [5]  [10, 1 , 0 , 8 , 3]
 
+  
+  for(let i = 1; i < array.length; i++){
+    let aux = array[i];
+    let j = i - 1;
+    while(array[j] > aux && j >= 0) {
+        array[j+1] = array[j];
+        j--;          
+    }
+    array[j+1] = aux;
+  }
+  return array;
 }
 
 
@@ -31,8 +68,24 @@ function selectionSort(array) {
   // Implementar el método conocido como selectionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
-  // Tu código:
+  // Tu código:   [20, 12, 50, 3, 44, 38, 5, 47, 15]
 
+
+ // let minimo = array[0];
+  for(let i = 0; i < array.length; i++){
+    let minimo = i; // minimo 0
+    for(let j = i+1; j < array.length; j++){ // j vale 12
+      if(array[j] < array[minimo]){ //
+        minimo = j;
+      }
+    }
+    if(i !== minimo){
+      let aux = array[i];
+      array[i] = array[minimo];
+      array[minimo] = aux;
+    }
+  }
+  return array;
 }
 
 
